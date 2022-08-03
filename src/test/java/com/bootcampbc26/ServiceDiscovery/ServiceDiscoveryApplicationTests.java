021e6f1d6a1bd56c049383c2408e6af329f2bf18
Katarin <kmendoro@emeal.nttdata.com>
2022-08-03 17:10:43 -0500

    first commit

---
+++ src/test/java/com/bootcampbc26/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampbc26.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
